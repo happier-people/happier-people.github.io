907e614cdd0e7c3e5a6090e8892ed3616889ca33
happier-people <shashkov.cr@gmail.com>
2017-04-28 00:39:19 +0300

    new index borders

--- services/happiness-index.js
+++ services/happiness-index.js
@@ -8,19 +8,19 @@ angular.module('myApp').service('hiService', [
         };
 
         function getMood (happinessIndex) {
-            if ((happinessIndex >= 0) && (happinessIndex < 0.25)) {
+            if ((happinessIndex >= 0) && (happinessIndex < 0.5)) {
                 return MOODS.CRYING;
             }
 
-            if ((happinessIndex >= 0.25) && (happinessIndex < 0.5)) {
+            if ((happinessIndex >= 0.5) && (happinessIndex < 0.55)) {
                 return MOODS.SAD;
             }
 
-            if ((happinessIndex >= 0.5) && (happinessIndex < 0.75)) {
+            if ((happinessIndex >= 0.55) && (happinessIndex < 0.6)) {
                 return MOODS.ALRIGHT;
             }
 
-            if ((happinessIndex >= 0.75) && (happinessIndex <= 1)) {
+            if ((happinessIndex >= 0.6) && (happinessIndex <= 1)) {
                 return MOODS.HAPPY;
             }
 
