cc6f25a60c600a314b1d8d4a9507d08021fcd38a
Eva <Eva>
2017-04-30 06:44:50 +0300

    updated intro overlay

--- app.js
+++ app.js
@@ -30,8 +30,24 @@ angular.module('myApp').controller('MainCtrl', [
             $scope.isOverlayShown = false;
         };
 
-        Array.prototype.diff = function(a) {
-            return this.filter(function(i) {return a.indexOf(i) < 0;});
-        };
+        $scope.greetings = [
+            "Marhaba", "Barev", "Heyello", "Kaixo", "Gruess gott",
+            "Namaskar", "Zdraveite", "Mingalarbar", "Hola", "Hafa adai",
+            "Nay ho", "Ni hau", "Mambo", "Boke", "Ahoj",
+            "Hej", "Goedendag", "Saluton", "Salut", "Dia duit",
+            "Gamardjoba", "Tag", "Kem che", "Yia sou", "Aloha",
+            "Shalom", "Namaste", "Magyar jo napot", "Nde-ewo", "Halo",
+            "Ciao", "Ohayou gozaimasu", "Namaskara", "Nook-neck", "Ahn nyeong ha se yo",
+            "Choni", "Sabaidee", "Salve", "Labdien", "Mbote",
+            "Laba diena", "Namaskkaram", "Kia ora", "Namaskar", "Niltze",
+            "Dumelang", "Hallo", "Salaam", "Dzien dobry", "Salut",
+            "Zdravstvuyte", "Talofa", "Salamaleikum", "Zdravo", "Dobry den",
+            "Zhivyo", "Howzit", "Jambo", "Hej", "Vanakkam",
+            "Baagunnara", "Bondia", "Li Ho", "Nong hao",
+            "Minjhani", "Merhaba", "Pryvit", "Adaab", "Xin chào",
+            "Shwmai", "Sholem aleikhem", "Sawubona", "Halo",
+            "Sannu", "Namascara", "Min-ga-la-ba", "Ngi ho", "Prannam"
+        ];
+        $scope.selectedGreeting = $scope.greetings [ Math.floor( Math.random() * $scope.greetings.length ) ];
     }
 ]);
