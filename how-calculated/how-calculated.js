8f996e8dfa569d909996cbb54dcc7cc40c9c4469
Eva <Eva>
2017-04-30 05:00:38 +0300

    slightly corrected but how

--- how-calculated/how-calculated.js
+++ how-calculated/how-calculated.js
@@ -3,12 +3,20 @@
 angular.module('myApp.howCalculated', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/how-it-was-calculated', {
-    templateUrl: 'how-calculated/how-calculated.html',
-    controller: 'HowCalculatedCtrl'
-  });
+    $routeProvider.when('/how-it-was-calculated', {
+        templateUrl: 'how-calculated/how-calculated.html',
+        controller: 'HowCalculatedCtrl'
+    });
+
 }])
 
-.controller('HowCalculatedCtrl', [function() {
-
-}]);
+.controller('HowCalculatedCtrl', [
+    'imagesService', 'spinnerService',
+    function (imagesService, spinnerService) {
+        spinnerService.start();
+        imagesService
+            .preloadImage('https://happynation.azurewebsites.net/Content/Diagram.png')
+            .then(function () {
+                spinnerService.stop();
+            });
+    }]);
